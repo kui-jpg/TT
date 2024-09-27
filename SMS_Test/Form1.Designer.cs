13e9a4a662a3f318118165b80c824419790b5ec5
kui-w <html_kui@163.com>
2024-09-27 14:12:54 +0800

    新建解决方案，添加winfor

---
+++ SMS_Test/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace SMS_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(-3, -2);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(711, 138);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title2.Name = "Title1";
+            this.chart1.Titles.Add(title2);
+            this.chart1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseDown);
+            this.chart1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseMove);
+            this.chart1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(707, 134);
+            this.ControlBox = false;
+            this.Controls.Add(this.chart1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.8D;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Red;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ SMS_Test/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SMS_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //初始化数据源
+            Random x = new Random();
+            Random y = new Random();
+            DateTime dttime = Convert.ToDateTime("2024-9-24 00:00");
+            List<DateTime> timelist = new List<DateTime>();
+
+            List<int> listGRT_Phase_Y = new List<int>();
+            List<int> listBD_Phase_Y = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                listGRT_Phase_Y.Add(y.Next(60, 74));
+                listBD_Phase_Y.Add(y.Next(30, 72));
+                timelist.Add(dttime.AddMinutes(x.Next(0, 4320)));
+            }
+
+
+
+            //设置图标XY轴网格线条样式
+            chart1.ChartAreas["ChartArea1"].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.NotSet;
+            chart1.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineDashStyle = ChartDashStyle.NotSet;
+            chart1.ChartAreas["ChartArea1"].AxisY.Maximum = 120;
+            chart1.ChartAreas["ChartArea1"].AxisY.Minimum = -120;
+            chart1.ChartAreas["ChartArea1"].Position.Auto = false;
+            chart1.ChartAreas["ChartArea1"].Position.X = 1;
+            chart1.ChartAreas["ChartArea1"].Position.Y = 80;
+            chart1.ChartAreas["ChartArea1"].Position.Width = 100;
+            chart1.ChartAreas["ChartArea1"].Position.Height = 90;
+
+            chart1.Legends[0].DockedToChartArea = "ChartArea1";
+            chart1.Legends[0].Docking = Docking.Bottom;
+
+
+            //添加Series
+            chart1.Series.Add(new Series("BD_Phase"));
+            chart1.Series["BD_Phase"].ToolTip = "#VALX,#VALY";
+            chart1.Series.Add(new Series("GRT_Phase"));
+            chart1.Series["GRT_Phase"].ToolTip = "#VALX,#VALY";
+
+
+            //绑定数据
+            for (int i = 0; i < timelist.Count - 1; i++)
+            {
+                chart1.Series["BD_Phase"].Points.AddXY(timelist[i].ToString("yy-MM-dd HH:mm"), listBD_Phase_Y[i].ToString());
+                chart1.Series["GRT_Phase"].Points.AddXY(timelist[i].ToString("yy-MM-dd HH:mm"), listGRT_Phase_Y[i].ToString());
+            }
+            //设置Series 样式
+            chart1.Series["BD_Phase"].ChartType = SeriesChartType.Line;
+            chart1.Series["BD_Phase"].Color = Color.DarkRed;
+            chart1.Series["GRT_Phase"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["GRT_Phase"].YAxisType = AxisType.Secondary;
+            chart1.Series["GRT_Phase"].Color = Color.Aquamarine;
+            chart1.ChartAreas["ChartArea1"].AxisY2.MajorGrid.LineDashStyle = ChartDashStyle.NotSet;
+            //chart1.ChartAreas["ChartArea1"].AxisX2.MajorGrid.LineDashStyle = ChartDashStyle.NotSet;
+            chart1.ChartAreas["ChartArea1"].AxisY2.Maximum = 84;
+            chart1.ChartAreas["ChartArea1"].AxisY2.Minimum = -84;
+
+
+            //添加Titles
+            Title title = new Title();
+            title.ForeColor = Color.CadetBlue;
+            title.Name = "chart1T";
+            title.Text = "GRT_Phase&BD_Phase";
+            chart1.Titles.Add(title);
+            chart1.Titles["chart1T"].Alignment = System.Drawing.ContentAlignment.TopCenter;
+            chart1.Titles["chart1T"].Position.Auto = false;
+            chart1.Titles["chart1T"].Position.X = 50;
+            chart1.Titles["chart1T"].Position.Y = 4;
+
+            //设置X轴Title属性
+            chart1.ChartAreas["ChartArea1"].AxisX.Title = "[" + dttime.ToString("yyyy-MM-dd HH:mm") + "~" + dttime.AddMinutes(4320).ToString("yyyy-MM-dd HH:mm") + "]";
+            chart1.ChartAreas["ChartArea1"].AxisX.TitleAlignment = StringAlignment.Near;
+            chart1.ChartAreas["ChartArea1"].AxisX.TitleForeColor = Color.Chocolate;
+
+            Title tt = new Title();
+            tt.Position.X = 20;
+
+        }
+
+
+        Point point;
+        bool isMoving = false;
+
+
+
+
+        private void chart1_MouseDown(object sender, MouseEventArgs e)
+        {
+            point = e.Location;
+            isMoving = true;
+        }
+
+        private void chart1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && isMoving)
+            {
+                Point pNew = new Point(e.Location.X - point.X, e.Location.Y - point.Y);
+                Location += new Size(pNew);
+            }
+        }
+
+        private void chart1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMoving = false;
+        }
+    }
+}
